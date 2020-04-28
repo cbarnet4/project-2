7f3c80e9aa8f26150917924ab4df083d0786a619
Cameron Barnett <cbarnet4@uoregon.edu>
2020-04-27 23:03:17 -0700

    Tried my best to do the reverse words functions. That was fun and special.

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,4 +1,61 @@
 // jshint esversion: 6
+let revWord = (str) => {
+    let arr = str.split('');
+    let result = '';
+    for (char of arr){
+        result = char + result;
+    }
+    return result;
+  };
+
+let revWords1 = (input) => {
+    separateWords = input.split(' ');
+    process = '';
+    for (let i = 0; i < separateWords.length; i++) {
+        process = '';
+        for (let j = separateWords[i].length  - 1; j >= 0; j--) {
+            process += separateWords[i][j];
+        }
+        separateWords[i] = process;
+        finalWords = separateWords.join(' ');
+    }
+    return finalWords;
+};
+
+let revWords2 = (input) => {
+  [input].forEach(x => {
+    let separateWords = input.split(' ');
+    process = '';
+    for (let i = 0; i < separateWords.length; i++) {
+        process = '';
+        for (let j = separateWords[i].length - 1; j >= 0; j--) {
+            process += separateWords[i][j];
+        }
+        separateWords[i] = process;
+        finalWords = separateWords.join(' ');
+    }
+  });
+    return finalWords;
+};
+
+let revWords3 = (input) => {
+  [input].forEach(x => {
+    separateWords = input.split(' ');
+    process = '';
+
+    for (let i = 0; i < separateWords.length; i++) {
+        process = '';
+        for (let j = separateWords[i].length - 1; j >= 0; j--) {
+            process += separateWords[i][j];
+        }
+    for (char of separateWords) {    //gave up on for/of.
+        separateWords[i] = process;
+        finalWords = separateWords.join(' ');
+    }
+  }
+  });
+    return finalWords;
+};
 
 let containsDuplicates = s => {
   let cArr = [0, 0, 0, 0],
